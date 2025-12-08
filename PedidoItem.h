528e7b07570b4291d3b0e2f8558ec1ef912df388
Jesus Bueno <A01425145.@tec.mx>
2025-12-08 14:48:17 -0600

    feat: agregada clase PedidoItem

---
+++ PedidoItem.h
@@ -0,0 +1,26 @@
+#ifndef PEDIDOITEM_H
+#define PEDIDOITEM_H
+#include <iostream>
+#include <string>
+#include "Producto.h"
+using namespace std;
+
+
+class PedidoItem{
+   private:
+    Producto producto;
+    int cantidad;
+    double precioUnitario;
+    double subtotal;
+public:
+    PedidoItem(Producto producto, int cantidad, double precioUnitario);
+
+    Producto getProducto();
+    int getCantidad();
+    double getPrecioUnitario();
+    double getSubtotal();
+    
+    double obtenerSubtotal();
+};
+
+#endif 
